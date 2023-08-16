ff479ff1670867eb0b17e4d39c507c873db835e8
Sanikwa <bornqueen22@gmail.com>
2023-08-16 15:27:10 -0400

    first commit mongodb

---
+++ models/veggies.js
@@ -0,0 +1,18 @@
+const veggies = [
+    {
+      name: "spinach",
+      color: "green",
+      readyToEat: true,
+    },
+    {
+      name: "onion",
+      color: "yellow",
+      readyToEat: true,
+    },
+    {
+      name: "eggplant",
+      color: "purple",
+      readyToEat: false,
+    }
+  ];
+module.exports = veggies;
---
+++ views/Index.jsx
@@ -0,0 +1,44 @@
+// Index.jsx
+const React = require('react');
+const Index =(props) => {
+    const { fruits } = props;
+        return (
+            <div>
+                <h1>Fruits Index Page</h1>
+                <ul>
+                    {fruits.map((fruit, i) => {
+                        return (
+                            <li>
+                                The {''}
+                                <a href={`/fruits/${i}`}>
+                                    {fruit.name}
+                                </a>{''}
+                                is {fruit.color} <br></br>
+                                {fruit.readyToEat?
+                                `It is ready to eat` : `Nope it is not ready`}
+                                <br />
+                            </li>
+                        );
+                    })};
+                </ul>
+                <nav>
+                    <a href="/fruits/new">Create a New Fruit</a>
+                </nav>
+            </div>
+        );
+    }
+module.exports = Index;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
